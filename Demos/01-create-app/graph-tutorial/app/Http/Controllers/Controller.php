39309015ae4dc12ffef270ec38464bb1924e5cfa
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-09-04 16:07:30 -0400

    Recreated 01 project with latest Laravel/Composer

--- Demos/01-create-app/graph-tutorial/app/Http/Controllers/Controller.php
+++ Demos/01-create-app/graph-tutorial/app/Http/Controllers/Controller.php
@@ -9,25 +9,25 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-  use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-  public function loadViewData()
-  {
-    $viewData = [];
-
-    // Check for flash errors
-    if (session('error')) {
-      $viewData['error'] = session('error');
-      $viewData['errorDetail'] = session('errorDetail');
-    }
-
-    // Check for logged on user
-    if (session('userName'))
+    public function loadViewData()
     {
-      $viewData['userName'] = session('userName');
-      $viewData['userEmail'] = session('userEmail');
-    }
+        $viewData = [];
 
-    return $viewData;
-  }
+        // Check for flash errors
+        if (session('error')) {
+            $viewData['error'] = session('error');
+            $viewData['errorDetail'] = session('errorDetail');
+        }
+
+        // Check for logged on user
+        if (session('userName'))
+        {
+            $viewData['userName'] = session('userName');
+            $viewData['userEmail'] = session('userEmail');
+        }
+
+        return $viewData;
+    }
 }
