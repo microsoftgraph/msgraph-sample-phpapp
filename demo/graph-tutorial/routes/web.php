9f9698d1bb8baf0d4b006c9dc3b82e530bbe73cd
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-03-30 15:53:42 -0400

    Completed add MS Graph step

--- demo/graph-tutorial/routes/web.php
+++ demo/graph-tutorial/routes/web.php
@@ -6,3 +6,4 @@ Route::get('/', 'HomeController@welcome');
 Route::get('/signin', 'AuthController@signin');
 Route::get('/callback', 'AuthController@callback');
 Route::get('/signout', 'AuthController@signout');
+Route::get('/calendar', 'CalendarController@calendar');
