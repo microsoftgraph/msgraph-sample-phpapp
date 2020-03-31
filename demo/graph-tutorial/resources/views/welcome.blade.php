e306f918ed5f3d5a946a981671b0f1276d299beb
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-03-31 10:32:13 -0400

    Update welcome.blade.php
    Revised welcome page text

--- demo/graph-tutorial/resources/views/welcome.blade.php
+++ demo/graph-tutorial/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="jumbotron">
   <h1>PHP Graph Tutorial</h1>
-  <p class="lead">This sample app shows how to use the Microsoft Graph API to access Outlook and OneDrive data from PHP</p>
+  <p class="lead">This sample app shows how to use the Microsoft Graph API to access a user's data from PHP</p>
   @if(isset($userName))
     <h4>Welcome {{ $userName }}!</h4>
     <p>Use the navigation bar at the top of the page to get started.</p>
