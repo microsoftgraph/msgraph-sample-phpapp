4501c794cbbe2a0be9daed6eb0d7445f2ca0c2b1
Jason Johnston <jasonjoh@users.noreply.github.com>
2021-01-25 17:17:21 -0500

    Update azure.php

--- demo/graph-tutorial/config/azure.php
+++ demo/graph-tutorial/config/azure.php
@@ -12,5 +12,5 @@ return [
   'scopes'            => env('OAUTH_SCOPES', ''),
   'authority'         => env('OAUTH_AUTHORITY', 'https://login.microsoftonline.com/common'),
   'authorizeEndpoint' => env('OAUTH_AUTHORIZE_ENDPOINT', '/oauth2/v2.0/authorize'),
-  'tokenEndpoint'     => env('OAUTH_TOKEN_ENDPOINT'),
+  'tokenEndpoint'     => env('OAUTH_TOKEN_ENDPOINT', '/oauth2/v2.0/token'),
 ];
