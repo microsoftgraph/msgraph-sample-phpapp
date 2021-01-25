5c2dc34ef5ea1ec928a615c91e7df32571acd581
Jason Johnston <jasonjoh@users.noreply.github.com>
2021-01-25 16:57:38 -0500

    Checking in completed project
    Recreated with PHP8/Laravel8.

---
+++ demo/graph-tutorial/config/azure.php
@@ -0,0 +1,16 @@
+<?php
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Access environment through the config helper
+// This will avoid issues when using Laravel's config caching
+// https://laravel.com/docs/8.x/configuration#configuration-caching
+return [
+  'appId'             => env('OAUTH_APP_ID', ''),
+  'appSecret'         => env('OAUTH_APP_SECRET', ''),
+  'redirectUri'       => env('OAUTH_REDIRECT_URI', ''),
+  'scopes'            => env('OAUTH_SCOPES', ''),
+  'authority'         => env('OAUTH_AUTHORITY', 'https://login.microsoftonline.com/common'),
+  'authorizeEndpoint' => env('OAUTH_AUTHORIZE_ENDPOINT', '/oauth2/v2.0/authorize'),
+  'tokenEndpoint'     => env('OAUTH_TOKEN_ENDPOINT'),
+];
