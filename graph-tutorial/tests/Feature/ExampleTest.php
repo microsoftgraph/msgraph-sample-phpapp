124b26b6e298bf666d88742b85190c30edd371d4
Jason Johnston <jasonjoh@users.noreply.github.com>
2022-08-22 14:15:10 -0400

    Update ExampleTest.php

--- graph-tutorial/tests/Feature/ExampleTest.php
+++ graph-tutorial/tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
+        $_SERVER['REQUEST_URI'] = '/';
         $response = $this->get('/');
 
         $response->assertStatus(200);
